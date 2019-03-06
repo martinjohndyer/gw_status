c9f983aa8c201ab986a78fa01126a581911055f1
Martin Dyer <martin.dyer@sheffield.ac.uk>
2019-03-06 16:03:23 +0000

    LIGO probide a JSON, that's nice

--- status_monitor.py
+++ status_monitor.py
@@ -1,44 +1,38 @@
 """A little script to monitor the GW detector statuses."""
 
+import json
+from datetime import datetime
 from time import sleep
 from urllib.request import urlopen
 
 from astropy.time import Time
 
-import bs4
-
-
-STATUS_PAGE = 'https://ldas-jobs.ligo.caltech.edu/~gwistat/gwistat/gwistat.html'
-
-DETECTORS = ['GEO 600',
-             'LIGO Hanford',
-             'LIGO Livingston',
-             'Virgo',
-             'KAGRA',
-             ]
-
-
-def parse_status_page(data):
-    """Parse the GW status page using BeautifulSoup."""
-    soup = bs4.BeautifulSoup(data, 'html.parser')
-    rows = soup.find_all('td')
-    data = [row.text.strip() for row in rows if row.text.strip()]
-    status_dict = {detector: data[data.index(detector) + 1] for detector in DETECTORS}
-    return status_dict
+STATUS_PAGE = 'https://ldas-jobs.ligo.caltech.edu/~gwistat/gwistat/gwistat.json'
 
 
 def get_gw_status():
     """Fetch and parse the GW status page."""
+    # Fetch the JSON
     request = urlopen(STATUS_PAGE)
-    data = request.read()
-    status_dict = parse_status_page(data)
-    return status_dict
+    contents = request.read()
+    data = json.loads(contents.decode())
+
+    # Need to parse the timestamp, doesn't include the year!
+    current_year = datetime.now().year
+    timestamp = str(current_year) + data['UTC']
+    timestamp = datetime.strptime(timestamp, '%Y%b %d, %H:%M UTC')
+    timestamp = Time(timestamp)  # Newer astropys gives Time.strptime
+
+    # Create the status dict
+    status_dict = {detector['site']: detector['status'] for detector in data['detectors']}
+
+    # Return both
+    return timestamp, status_dict
 
 
-def format_status(status_dict):
+def format_status(time, status_dict):
     """Format the status dict."""
-    now = Time.now()
-    string = 'Status at {}:\n'.format(now.iso)
+    string = 'Status at {}:\n'.format(time.iso)
     for detector in sorted(status_dict):
         string += '\t{: <{i}}: "{}"\n'.format(detector, status_dict[detector],
                                               i=max([len(d) for d in status_dict]) + 1)
@@ -47,11 +41,11 @@ def format_status(status_dict):
 
 def listen():
     """Listen to the status page and pick up any changes."""
-    status_dict = get_gw_status()
-    print(format_status(status_dict))
+    time, status_dict = get_gw_status()
+    print(format_status(time, status_dict))
     while True:
         old_status_dict = status_dict.copy()
-        status_dict = get_gw_status()
+        time, status_dict = get_gw_status()
         changed = [detector for detector in status_dict
                    if status_dict[detector] != old_status_dict[detector]]
         if changed:
@@ -60,7 +54,7 @@ def listen():
                                                                    old_status_dict[detector],
                                                                    status_dict[detector],
                                                                    ))
-            print(format_status(status_dict))
+            print(format_status(time, status_dict))
         sleep(30)
 
 
