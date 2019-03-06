863030d933642c2ea7f096e9f74e6c7df2acfa1e
Martin Dyer <martin.dyer@sheffield.ac.uk>
2019-03-06 11:54:11 +0000

    Add status listener

--- status_monitor.py
+++ status_monitor.py
@@ -1,26 +1,55 @@
 """A little script to monitor the GW detector statuses."""
 
-import subprocess
+from time import time, sleep
+from urllib.request import urlopen
+
+import bs4
+from astropy.time import Time
 
 
 STATUS_PAGE = 'https://ldas-jobs.ligo.caltech.edu/~gwistat/gwistat/gwistat.html'
 
+DETECTORS = ['GEO 600',
+             'LIGO Hanford',
+             'LIGO Livingston',
+             'Virgo',
+             'KAGRA',
+             ]
 
-def curl_data_from_url(url, wait_time=5, outfile=None):
-    """Fetch data from a URL."""
-    if not outfile:
-        outfile = '/tmp/curlout'
-    curl_command = 'curl -s -m {:.0f}'.format(wait_time)
-    curl_command += ' -o {}'.format(outfile)
-    curl_command += ' {}'.format(url)
-    try:
-        p = subprocess.Popen(curl_command, shell=True, close_fds=True)
-        p.wait()
-    except Exception:
-        print('Error fetching URL "{}"'.format(url))
 
-    with open(outfile, 'r') as f:
-        data = f.read()
+def parse_status_page(data):
+    """Parse the GW status page using BeautifulSoup."""
+    soup = bs4.BeautifulSoup(data, 'html.parser')
+    rows = soup.find_all('td')
+    data = [row.text.strip() for row in rows if row.text.strip()]
+    status_dict = {detector: data[data.index(detector) + 1] for detector in DETECTORS}
+    return status_dict
 
-    return data
 
+def get_gw_status():
+    """Fetch and parse the GW status page."""
+    request = urlopen(STATUS_PAGE)
+    data = request.read()
+    status_dict = parse_status_page(data)
+    return status_dict
+
+
+def format_status(status_dict):
+    now = Time.now()
+    string = 'Status at {}:\n'.format(now.iso)
+    for detector in sorted(status_dict):
+        string += '\t{}: "{}"\n'.format(detector, status_dict[detector])
+    return string
+
+def listen():
+    """Listen to the status page and pick up any changes."""
+    status_dict = get_gw_status()
+    print(format_status(status_dict))
+    while True:
+        old_status_dict = status_dict.copy()
+        status_dict = get_gw_status()
+        changed = [detector for detector in status_dict
+                   if status_dict[detector] != old_status_dict[detector]]
+        if changed:
+            print(format_status(status_dict))
+        time.sleep(30)
